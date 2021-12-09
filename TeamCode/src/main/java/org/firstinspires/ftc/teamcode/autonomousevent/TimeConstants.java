8a370639d5757252ca9a4f5396c3ecfd96716a76
sudzoof <sunjoshua2@gmail.com>
2021-12-09 15:10:26 -0500

    Let TimeConstants functions accept doubles

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousevent/TimeConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousevent/TimeConstants.java
@@ -3,11 +3,11 @@ package org.firstinspires.ftc.teamcode.autonomousevent;
 public class TimeConstants {
     public static final int DEGREE_90 = 660;
 
-    public static int degreeTime(int degree){
+    public static int degreeTime(double degree){
         return 0;
     }
 
-    public static int distanceTime(int distance){
+    public static int distanceTime(double distance){
         return 0;
     }
 
