466e85dc089cf70f0320770974ab427974845fd0
sudzoof <sunjoshua2@gmail.com>
2021-12-09 15:07:40 -0500

    Created static class that just returns converted values

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousevent/TimeConstants.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.autonomousevent;
+
+public class TimeConstants {
+    public static final int DEGREE_90 = 660;
+
+    public static int degreeTime(int degree){
+        return 0;
+    }
+
+    public static int distanceTime(int distance){
+        return 0;
+    }
+
+}
