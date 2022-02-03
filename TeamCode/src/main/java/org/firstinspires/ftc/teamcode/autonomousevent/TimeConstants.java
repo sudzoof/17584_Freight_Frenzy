dd8ca70db71cdfe3eb38d07e8be0f19327b32377
sudzoof <sunjoshua2@gmail.com>
2022-02-03 15:03:11 -0500

    Finalized(?) time constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousevent/TimeConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousevent/TimeConstants.java
@@ -1,14 +1,25 @@
 package org.firstinspires.ftc.teamcode.autonomousevent;
 
 public class TimeConstants {
-    public static final int DEGREE_90 = 660;
+    // 700 was pretty clean but had a very slight increase in degree
+    public static final double DEGREE_90 = 700;
 
-    public static int degreeTime(double degree){
-        return 0;
+    public static double degreeTime(int degree){
+        switch(degree){
+            case 90:
+                return DEGREE_90;
+            default:
+                return degreeTime((double)degree);
+        }
     }
 
-    public static int distanceTime(double distance){
-        return 0;
+    public static double degreeTime(double degree){
+        return DEGREE_90 * degree / 90;
+    }
+
+
+    public static double distanceTime(double distance){
+        return 1000.0 * distance / 22 ;
     }
 
 }
